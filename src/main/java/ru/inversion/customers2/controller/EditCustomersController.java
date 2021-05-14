fb5a2e036a129614a1207d00e7fac44336bfb27e
Ivan <ivan.vaschenko@inversion.ru>
2021-05-14 15:40:18 +0300

    add EditCustomers.fxml

---
+++ src/main/java/ru/inversion/customers2/controller/EditCustomersController.java
@@ -0,0 +1,12 @@
+package ru.inversion.customers2.controller;
+
+import ru.inversion.customers2.pojo.PCustomers;
+import ru.inversion.fx.form.JInvFXBrowserController;
+import ru.inversion.fx.form.JInvFXFormController;
+
+public class EditCustomersController extends JInvFXFormController<PCustomers> {
+    @Override
+    protected void init() throws Exception {
+        super.init();
+    }
+}
